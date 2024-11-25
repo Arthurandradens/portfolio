4a791c52b683ba1cff9f85ece9c7a59d69d4b516
arthur andrade nunes <nunesarthur252@gmail.com>
2024-11-25 20:07:40 -0300

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
     <div class="border-b border-neutral-900 pb-20">
         <h1 class="my-10 text-center text-4xl">Fale comigo</h1>
         <div class="text-center tracking-tighter">
-            <a href="#" class="border-b">{{ contact.mail }}</a>
+            <a href="mailto:{{ contact.mail }}" class="border-b">{{ contact.mail }}</a>
             <p class="my-4">{{ contact.phone}}</p>
         </div>
     </div>
